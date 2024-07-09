06680332f38f2c5931b1b1e28244549ee80f7243
kosei-matsubara <ma24.tu02.se18@gmail.com>
2024-07-09 20:58:31 +0900

    feat: MUIを導入する

---
+++ front_end/src/pages/hello_mui.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@mui/material'
+import type { NextPage } from 'next'
+
+const HelloMui: NextPage = () => {
+  return (
+    <div>
+      <button>Hello Mui@V5!</button>
+    </div>
+  )
+}
+
+export default HelloMui
