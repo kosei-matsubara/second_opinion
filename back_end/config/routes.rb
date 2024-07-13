ee432c268305cc96b6ed5ff376c6eb4e6fe3f648
kosei-matsubara <ma24.tu02.se18@gmail.com>
2024-07-13 21:00:53 +0900

    feat: 投稿新規作成を追加する

--- back_end/config/routes.rb
+++ back_end/config/routes.rb
@@ -9,7 +9,9 @@ Rails.application.routes.draw do
 
       namespace :current do
         resource :user, only: [:show]
+        resources :articles, only: [:create, :update]
       end
+      resources :articles, only: [:index, :show]
     end
   end
 end
