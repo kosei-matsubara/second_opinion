d50ad5deb1a35519277166a5aa60603f0d39bd8c
kosei-matsubara <ma24.tu02.se18@gmail.com>
2024-09-10 22:58:44 +0900

    feat: AWS設定ファイルを追加する

--- back_end/entrypoint.prod.sh
+++ back_end/entrypoint.prod.sh
@@ -6,8 +6,8 @@ echo "Start entrypoint.prod.sh"
 echo "rm -f /myapp/tmp/pids/server.pid"
 rm -f /myapp/tmp/pids/server.pid
 
-echo "bundle exec rails db:create RAILS_ENV=production"
-bundle exec rails db:create RAILS_ENV=production
+# echo "bundle exec rails db:create RAILS_ENV=production"
+# bundle exec rails db:create RAILS_ENV=production
 
 echo "bundle exec rails db:migrate RAILS_ENV=production"
 bundle exec rails db:migrate RAILS_ENV=production
