6f4cd47cb7188de9c25efc1b9177d4074a4a8125
kosei-matsubara <ma24.tu02.se18@gmail.com>
2024-10-10 20:28:38 +0900

    refactor: コメントを修正する

--- back_end/app/controllers/application_controller.rb
+++ back_end/app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::API
   protected
 
   def configure_permitted_parameters
-    # devise_token_authは新規userアカウントを登録時、デフォルトはemailとpasswordをuserレコードに登録するためnameを追加する
+    # devise_token_authは新規userアカウントを登録時、デフォルトでemailとpasswordをuserレコードに登録するためnameを追加する
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
