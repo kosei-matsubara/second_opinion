ee962ae3e3459211e4c50b215f0b41db7da6100b
kosei-matsubara <ma24.tu02.se18@gmail.com>
2024-10-30 20:54:37 +0900

    refactor: テスト内容を修正する

--- back_end/spec/requests/api/v1/articles_spec.rb
+++ back_end/spec/requests/api/v1/articles_spec.rb
@@ -73,7 +73,7 @@ RSpec.describe "Api::V1::Articles", type: :request do
       end
     end
 
-    context "article_id に対応する articles レコードが存在しない時" do
+    context "article_id に対応する「articles」レコードが存在しない時" do
       let(:article_id) { 10_000_000_000 }
 
       it "ActiveRecord::RecordNotFound エラーが返る" do
