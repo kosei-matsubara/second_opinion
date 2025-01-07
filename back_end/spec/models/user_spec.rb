834db5c4066e2744dbbab60b1819f22461be8771
kosei-matsubara <ma24.tu02.se18@gmail.com>
2025-01-07 16:42:58 +0900

    refactor: テスト内容を修正する

--- back_end/spec/models/user_spec.rb
+++ back_end/spec/models/user_spec.rb
@@ -18,24 +18,69 @@ RSpec.describe User, type: :model do
   end
 
   describe "enumをテストする" do
-    # context "user_division" do
-    #   it "policyholderが正しく定義されている" do
-    #     expect(User.user_divisions[:policyholder]).to eq(10)
-    #   end
+    context "user_division" do
+      it "policyholderが定義通りである" do
+        expect(User.user_divisions[:policyholder]).to eq(10)
+      end
 
-    #   it "insurance_agentが正しく定義されている" do
-    #     expect(User.user_divisions[:insurance_agent]).to eq(20)
-    #   end
-    # end
+      it "insurance_agentが定義通りである" do
+        expect(User.user_divisions[:insurance_agent]).to eq(20)
+      end
+    end
 
-    # context "sex" do
-    #   it "maleが正しく定義されている" do
-    #     expect(User.sexes[:male]).to eq(10)
-    #   end
+    context "sex" do
+      it "maleが定義通りである" do
+        expect(User.sexes[:male]).to eq(10)
+      end
 
-    #   it "femaleが正しく定義されている" do
-    #     expect(User.sexes[:female]).to eq(20)
-    #   end
-    # end
+      it "femaleが定義通りである" do
+        expect(User.sexes[:female]).to eq(20)
+      end
+    end
+
+    context "generation" do
+      it "teensが定義通りである" do
+        expect(User.generations[:teens]).to eq(10)
+      end
+
+      it "twentiesが定義通りである" do
+        expect(User.generations[:twenties]).to eq(20)
+      end
+
+      it "thirtiesが定義通りである" do
+        expect(User.generations[:thirties]).to eq(30)
+      end
+
+      it "fortiesが定義通りである" do
+        expect(User.generations[:forties]).to eq(40)
+      end
+
+      it "fiftiesが定義通りである" do
+        expect(User.generations[:fifties]).to eq(50)
+      end
+
+      it "sixties_and_aboveが定義通りである" do
+        expect(User.generations[:sixties_and_above]).to eq(60)
+      end
+    end
+
+    context "family_structure" do
+      it "singleが定義通りである" do
+        expect(User.family_structures[:single]).to eq(10)
+      end
+
+      it "coupleが定義通りである" do
+        expect(User.family_structures[:couple]).to eq(20)
+      end
+
+      it "couple_with_childrenが定義通りである" do
+        expect(User.family_structures[:couple_with_children]).to eq(30)
+      end
+    end
+  end
+
+  describe "validateをテストする" do
+    context "factoryのデフォルト設定の場合" do
+    end
   end
 end
