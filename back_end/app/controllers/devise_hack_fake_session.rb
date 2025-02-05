2f46093ef358135979c1dc00b64b798b98746861
kosei-matsubara <ma24.tu02.se18@gmail.com>
2025-02-05 15:03:44 +0900

    refactor:処理コメントを追加する

--- back_end/app/controllers/devise_hack_fake_session.rb
+++ back_end/app/controllers/devise_hack_fake_session.rb
@@ -2,10 +2,12 @@ module DeviseHackFakeSession
   extend ActiveSupport::Concern
 
   class FakeSession < Hash
+    # セッションを無効にする
     def enabled?
       false
     end
 
+    # セッションの削除を無効化する
     def destroy; end
   end
 
@@ -15,7 +17,9 @@ module DeviseHackFakeSession
     private
 
     def set_fake_session
+      # RailsがAPIモードで動作する場合に処理を実行する
       if Rails.configuration.respond_to?(:api_only) && Rails.configuration.api_only
+        # APIモードではrack.sessionは存在しないがDeviseがrack.sessionを要求する場合があるため、それを回避するために代わりにFakeSessionを設定する
         request.env["rack.session"] ||= ::DeviseHackFakeSession::FakeSession.new
       end
     end
